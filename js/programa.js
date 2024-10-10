d069d0221e785525a165070fe11b51a301d7adb3
Agustin <nasampaolesi@gmail.com>
2024-10-10 20:21:48 -0300

    segunda entrega

--- js/programa.js
+++ js/programa.js
@@ -12,7 +12,7 @@
 
 
 
-
+let fechaActual = new Date()
 const usuariosHabilitados = ["david", "marco", "coderhouse"]
 
 let usuario
@@ -40,13 +40,17 @@ do {
 
 
 
-
 function calcularSuma() {
     // Obtener los valores de los inputs
     const importe1 = parseFloat(document.getElementById('tramiteJubilatorio').value) || 0
     const importe2 = parseFloat(document.getElementById('calculosPrevisionales').value) || 0
     const importe3 = parseFloat(document.getElementById('reajusteJubilatorio').value) || 0
     const adicional = document.getElementById('facturaConsultaGeneral').checked ? 15000 : 0
+
+    if (importe1 < 0 || importe2 < 0 || importe3 < 0) {
+        alert("No se pueden ingresar valores negativos")
+        return
+    }
 
     // Calcular la suma total
     const total = importe1 + importe2 + importe3 + adicional
@@ -58,14 +62,121 @@ function calcularSuma() {
 
     const costosservicios = [importe1, importe2, importe3, adicional]
 
-    let importetotal = 0
+    let importetotal = 0;
     for (let i = 0; i < servicios.length; i++) {
 
-        console.log(servicios[i] + " " + costosservicios[i])
+        //console.log(servicios[i] + " " + costosservicios[i])
 
-        importetotal = importetotal + costosservicios[i]
+        importetotal += costosservicios[i];
 
     }
-    console.log("El importe final a pagar es $" + importetotal)
+    //console.log("El importe final a pagar es $" + importetotal)
+    //console.log("El cliente es Id:" + cliente.id + " " + cliente.nombre + " " + cliente.apellido)
 }
+
+
+
+class Cliente {
+    static id = 0    //propiedad estátia
+
+    constructor(nombre, apellido, dni, telefono, email) {
+        this.id = ++Cliente.id
+        this.nombre = nombre
+        this.apellido = apellido
+        this.dni = dni
+        this.telefono = telefono
+        this.email = email
+    }
+
+    confirmar() {
+        /* console.log("Se ha registrado el cliente " + this.nombre + " " + this.apellido + " con Id " + this.id + " DNI: " + this.dni +
+            " Telefono: " + this.telefono + " E-mail: " + this.email + " con fecha " + fechaActual.getDate() + "/" + (fechaActual.getMonth() + 1) + "/" + fechaActual.getFullYear() +
+            " a las " + fechaActual.getHours() + ":" + fechaActual.getMinutes() + ":" + fechaActual.getSeconds()) */
+
+        alert("Se ha registrado el cliente " + this.nombre + " " + this.apellido + " con Id " + this.id + " DNI: " + this.dni +
+            " Telefono: " + this.telefono + " E-mail: " + this.email + " con fecha " + fechaActual.getDate() + "/" + (fechaActual.getMonth() + 1) + "/" + fechaActual.getFullYear() +
+            " a las " + fechaActual.getHours() + ":" + fechaActual.getMinutes() + ":" + fechaActual.getSeconds())
+    }
+}
+
+
+// Solicitar datos del cliente por pantalla
+const nombre = prompt("Ingrese su nombre:");
+const apellido = prompt("Ingrese su apellido:");
+const dni = prompt("Ingrese su DNI:");
+const telefono = prompt("Ingrese su teléfono:");
+const email = prompt("Ingrese su email:");
+
+// Crear una instancia de la clase Cliente con los datos ingresados
+const cliente = new Cliente(nombre, apellido, dni, telefono, email);
+
+// Confirmar los datos del cliente
+cliente.confirmar();
+//console.log("el cliente tiene id " + cliente.id)
+//const cliente = new Cliente("Juan", "Perez", 12345678, 1122334455, "lalala@gmail.com")
+
+//cliente.confirmar() 
+
+
+const stockProductos = [
+    {
+        id: 1,
+        nombre: "Accidentes Laborales ",
+        precio: 10000,
+        desc: "Servicio Disponible.",
+        img:"../img/AccidentesLaborales.jpg"
+
+    },
+    {
+        id: 2,
+        nombre: "Despido Discriminatorio",
+        precio: 20000,
+        desc: "Servicio Disponible.",
+        img: "../img/DespidoDiscriminatorio.jpg"
+    },
+    {
+        id: 3,
+        nombre: "Despido Injustificado",
+        precio: 15000,
+        desc: "Producto en venta.",
+        img: "../img/DespidoInjustificado.jpg"
+    },
+    {
+        id: 4,
+        nombre: "Jubilaciones y Pensiones",
+        precio: 10000,
+        desc: "Producto en venta.",
+        img: "../img/jubilacionesYPensiones.jpg"
+    },
+    {
+        id: 5,
+        nombre: "Liquidacion Blue Corp",
+        precio: 25000,
+        desc: "Producto en venta.",
+        img: "../img/LiquidacionBlueCorp.jpg"
+    },
+
+]
+
+
+const contenedorProductos = document.getElementById("contenedor-productos")
+
+stockProductos.forEach((elm) => {
+
+    const div = document.createElement("div")
+
+    div.classList.add("producto")
+
+    div.innerHTML = `
+    <img src="${elm.img}" >
+    <hr>
+    <h3>${elm.nombre}</h3>
+    <p>Precio: $${elm.precio}</p>
+
+    <button>Contratar</button>
+    `
+
+    contenedorProductos.appendChild(div)
+})
+
 
